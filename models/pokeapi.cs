54ef3d8ad3f96e61a00f9f3316e1fce43ac6c7ad
Ana Cecília <122322406+ana-ceci@users.noreply.github.com>
2025-10-29 21:22:25 -0300

    Add files via upload

---
+++ models/pokeapi.cs
@@ -0,0 +1,17 @@
+namespace BlazorPokeApi.models
+{
+    public class Pokemon
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int height { get; set; }
+        public int weight { get; set; }
+
+        public Sprites sprites { get; set; } 
+    }
+
+    public class Sprites
+    {
+        public string front_default { get; set; } 
+    }
+}
